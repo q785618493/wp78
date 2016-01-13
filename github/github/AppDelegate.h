153308a63e3a97eec30186b7745578a46996285a
q785618493 <785618493@qq.com>
2016-01-13 11:45:35 +0800

    第一次添加

---
+++ github/github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 ftzs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github/github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  github
+//
+//  Created by ma c on 16/1/13.
+//  Copyright © 2016年 ftzs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
